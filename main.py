400d637c332e12d5faf12c0c33eddd65457db12e
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-07-27 11:23:15 -0400

    Update main.py (#8)

--- main.py
+++ main.py
@@ -30,10 +30,27 @@ except Exception as e:
 def process_event(request):
     try:
         event = request.get_json()
-        if 'message' in event:
-            return handle_message(event['message'])
+        event_type = event['type']
+
+        if event_type == 'ADDED_TO_SPACE':
+            return jsonify({'text': 'Hello! I am your Chat bot. How can I assist you today?'})
+
+        elif event_type == 'MESSAGE':
+            message = event['message']
+            user_message = message['text']
+
+            # Check if the bot was mentioned in the room, if so, remove the mention
+            if 'annotations' in message:
+                for annotation in message['annotations']:
+                    if annotation['type'] == 'USER_MENTION':
+                        if annotation['userMention']['user']['name'] == event['space']['name']:
+                            user_message = user_message.replace(annotation['userMention']['text'], '').strip()
+
+            return handle_message(user_message)
+
         else:
-            return jsonify({'text': 'Sorry, I can only process text messages.'})
+            return jsonify({'text': 'Sorry, I can only process messages and being added to a space.'})
+
     except Exception as e:
         print(f"Error processing event: {str(e)}")
         return jsonify({'text': 'Sorry, I encountered an error while processing your message.'})
