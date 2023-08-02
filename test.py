3a65df333c2e658fcd3049345514ff301e9bab80
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-08-02 01:43:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import unittest
 from unittest.mock import patch, MagicMock
 import datetime
-from main import handle_message, user_sessions, turn_counts, last_received_times, MAX_TURNS, TTL
+from main import handle_message, process_event, user_sessions, turn_counts, last_received_times
 
-class TestHandleMessage(unittest.TestCase):
+class TestMain(unittest.TestCase):
     @patch('main.AIChat')
-
     def test_max_turns(self, mock_aichat):
         user_id = 'test_user'
         user_message = 'Hello, bot!'
+        MAX_TURNS = 5
         mock_aichat.return_value = MagicMock(**{'__call__.return_value': 'Hello, user!'})
 
         # Send MAX_TURNS + 1 messages
@@ -22,11 +22,10 @@ class TestHandleMessage(unittest.TestCase):
 
     @patch('main.AIChat')
     @patch('main.datetime')
-
-  
     def test_ttl_expiration(self, mock_datetime, mock_aichat):
         user_id = 'test_user'
         user_message = 'Hello, bot!'
+        TTL = 600
         mock_aichat.return_value = MagicMock(**{'__call__.return_value': 'Hello, user!'})
 
         # First message
@@ -45,6 +44,30 @@ class TestHandleMessage(unittest.TestCase):
         self.assertEqual(turn_counts.get(user_id), 1)  # Turn count has been reset
         self.assertEqual(last_received_times.get(user_id), datetime.datetime(2022, 1, 1, 0, 11))  # Last received time has been updated
 
+    @patch('main.AIChat')
+    def test_process_event(self, mock_aichat):
+        event_data_message = {
+            'type': 'MESSAGE',
+            'user': {'name': 'test_user'},
+            'message': {'text': 'Hello, bot!'}
+        }
+        mock_aichat.return_value = MagicMock(**{'__call__.return_value': 'Hello, user!'})
+        response = process_event(MagicMock(get_json=MagicMock(return_value=event_data_message)))
+        self.assertEqual(response.get_json(), {'text': 'Hello, user!'})
+
+        event_data_added_to_space = {
+            'type': 'ADDED_TO_SPACE',
+            'user': {'name': 'test_user'}
+        }
+        response = process_event(MagicMock(get_json=MagicMock(return_value=event_data_added_to_space)))
+        self.assertEqual(response.get_json(), {'text': 'Hello! I am your Chat bot. How can I assist you today?'})
+
+        event_data_unknown_type = {
+            'type': 'UNKNOWN_TYPE',
+            'user': {'name': 'test_user'}
+        }
+        response = process_event(MagicMock(get_json=MagicMock(return_value=event_data_unknown_type)))
+        self.assertEqual(response.get_json(), {'text': 'Sorry, I can only process messages and being added to a space.'})
 
 if __name__ == '__main__':
     unittest.main()
