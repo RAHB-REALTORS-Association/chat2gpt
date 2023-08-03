c68a1b032c5bd3e725f547575b568d859db7f5e6
Justin Hayes <justinh@rahb.ca>
2023-08-03 14:57:50 -0400

    Fix tokenizer encoding

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ last_received_times = {}  # A dictionary to track the last received time for eac
 
 
 # Define the function for token counting
-def num_tokens_from_string(string: str, model_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(model_name)
+def num_tokens_from_string(string: str) -> int:
+    encoding = tiktoken.get_encoding("cl100k_base")
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
