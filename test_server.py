1e7c40be089059bbccc0c7bb277b4b423134e985
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-02 18:39:36 -0400

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from main import process_event
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+@app.route('/')
+def root():
+    return send_from_directory('.', 'test_interface.html')
 
 @app.route('/post', methods=['POST'])
 def google_chat_event():
