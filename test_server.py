7fb824e67528e6c9284862476baa7de4b311a5ef
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-02 22:19:46 -0400

    HTMX < jQuery

--- test_server.py
+++ test_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 from main import process_event
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
@@ -10,6 +11,16 @@ def root():
 @app.route('/post', methods=['POST'])
 def google_chat_event():
     try:
+        # Debug: Print the raw request data
+        print("Raw Request Data:", request.data)
+
+        # Debug: Try to load JSON manually
+        try:
+            json.loads(request.data)
+            print("JSON is well-formed")
+        except json.JSONDecodeError as e:
+            print("JSON Decode Error:", e)
+
         # Get the event data from the request body
         response = process_event(request)
         return response
@@ -17,5 +28,4 @@ def google_chat_event():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000)
-    
+    app.run(port=5000, debug=True)
