ee07da67882e07dba9f9a6f061cb07202c575dc1
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-02 22:22:23 -0400

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -14,13 +14,6 @@ def google_chat_event():
         # Debug: Print the raw request data
         print("Raw Request Data:", request.data)
 
-        # Debug: Try to load JSON manually
-        try:
-            json.loads(request.data)
-            print("JSON is well-formed")
-        except json.JSONDecodeError as e:
-            print("JSON Decode Error:", e)
-
         # Get the event data from the request body
         response = process_event(request)
         return response
