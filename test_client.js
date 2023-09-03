a26a17bc23b48852e1aa99705e5983d236b0618c
justinh <me@justin-hayes.com>
2023-09-03 02:46:55 -0400

    Test interface improvements

---
+++ test_client.js
@@ -0,0 +1,23 @@
+$("#userForm").submit(function(e) {
+    e.preventDefault();
+    const userInput = $("#userInput").val();
+    const wrappedEventData = {
+        "type": "MESSAGE",
+        "user": { "name": "users/simulated" },
+        "message": { "text": userInput }
+    };
+    $.ajax({
+        url: "/post",
+        type: "POST",
+        data: JSON.stringify(wrappedEventData),
+        contentType: "application/json",
+        success: function(response) {
+            // Handle the response
+            $("#responseContainer").html("<h6>Response:</h6><pre>" + JSON.stringify(response, null, 2) + "</pre>");
+        },
+        error: function(error) {
+            // Handle the error
+            $("#responseContainer").html("<h6>Response:</h6><pre>" + JSON.stringify(error, null, 2) + "</pre>");
+        }
+    });
+});
