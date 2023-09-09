d1fa327ea24ed6f041975b08577ef14e5c37c5fc
justinh <me@justin-hayes.com>
2023-09-09 02:43:13 -0400

    Add spinner to send

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 $("#userForm").submit(function(e) {
     e.preventDefault();
+    
+    // Show spinner and disable button
+    $("#spinner").show();
+    $("#submitButton").prop("disabled", true);
+    
     const userInput = $("#userInput").val();
     const wrappedEventData = {
         "type": "MESSAGE",
@@ -17,11 +22,16 @@ $("#userForm").submit(function(e) {
             const htmlFormattedText = marked.parse(markdownText);
             $("#formatted").html(htmlFormattedText);
             $("#raw").html('<pre>' + JSON.stringify(response, null, 2) + '</pre>');
-        },              
+        },
         error: function(error) {
             // Handle the error
             $("#formatted").html(error);
             $("#raw").html("<pre>" + JSON.stringify(error, null, 2) + "</pre>");
+        },
+        complete: function() {
+            // Hide spinner and re-enable button
+            $("#spinner").hide();
+            $("#submitButton").prop("disabled", false);
         }
     });
 });
