f003e22a2e3d1c2527e3b2b446365990cfc423e8
justinh <me@justin-hayes.com>
2023-09-09 13:21:27 -0400

    Log to file

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 from flask import Flask, request, jsonify, send_from_directory
 from main import process_event
 import json
+import logging
+
+LOG_FILE = "chat2gpt-server-log.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] - %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE),
+        logging.StreamHandler()
+    ]
+)
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
@@ -12,16 +24,17 @@ def root():
 def google_chat_event():
     try:
         # Log the raw request data for debugging
-        print("Request Data:", request.data)
+        logging.info("Request Data: %s", request.data)
 
         # Get the event data from the request body
         response = process_event(request)
 
         # Log the raw request data for debugging
-        print("Response Data:", json.dumps(response.get_json(), indent=4))
+        logging.info("Response Data: %s", json.dumps(response.get_json(), indent=4))
 
         return response
     except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
